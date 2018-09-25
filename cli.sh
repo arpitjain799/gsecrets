49d30ab15b3b7c6338eeb844f4d432cb26b8e4f3
Marc-Antoine <marc@rvit.co>
2018-09-24 21:13:43 -0700

    Add whitespace stripping to docker cli wrapper

--- cli.sh
+++ cli.sh
@@ -1,2 +1,8 @@
-echo "Running gsecrets inside Docker container"
-docker-compose run --rm gsecrets "$@"
+# echo "Running gsecrets inside Docker container"
+
+SCRIPT_PATH=`greadlink -f ${BASH_SOURCE[0]} || readlink -f ${BASH_SOURCE[0]}`
+cd `dirname ${SCRIPT_PATH}` > /dev/null
+
+OUTPUT="$(docker-compose run --rm gsecrets $@)"
+OUTPUT_NO_TRAIL_SPACE="$(echo "${OUTPUT}" | sed -e 's/[[:space:]]*$//')"
+echo $OUTPUT_NO_TRAIL_SPACE
