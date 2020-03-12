1936dd3f748e8f0abe15f94858ca457c95307034
Steve <steve@recurve.com>
2020-03-12 11:11:36 -0400

    Bump version
    Signed-off-by: Steve <steve@recurve.com>

--- setup.py
+++ setup.py
@@ -5,16 +5,17 @@ from shutil import rmtree
 
 here = os.path.abspath(os.path.dirname(__file__))
 
+
 class UploadCommand(Command):
     """Support setup.py upload."""
 
-    description = 'Build and publish the package.'
+    description = "Build and publish the package."
     user_options = []
 
     @staticmethod
     def status(s):
         """Prints things in bold."""
-        print('\033[1m{0}\033[0m'.format(s))
+        print("\033[1m{0}\033[0m".format(s))
 
     def initialize_options(self):
         pass
@@ -24,40 +25,50 @@ class UploadCommand(Command):
 
     def run(self):
         try:
-            self.status('Removing previous builds...')
-            rmtree(os.path.join(here, 'dist'))
+            self.status("Removing previous builds...")
+            rmtree(os.path.join(here, "dist"))
         except OSError:
             pass
 
-        self.status('Building Source and Wheel (universal) distribution...')
-        os.system('{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
+        self.status("Building Source and Wheel (universal) distribution...")
+        os.system("{0} setup.py sdist bdist_wheel --universal".format(sys.executable))
 
-        self.status('Uploading the package to PyPi via Twine...')
-        os.system('twine upload dist/*')
+        self.status("Uploading the package to PyPi via Twine...")
+        os.system("twine upload dist/*")
 
         sys.exit()
 
-setup(name='gsecrets',
-      version='1.1.1',
-      description='API and CLI for securely managing secrets',
-      url='https://github.com/openeemeter/gsecrets',
-      author='Open Energy Efficiency',
-      packages=find_packages(),
-      install_requires=[
-        'click==7.0',
-        'google-api-python-client==1.7.11',
-        'google-cloud-storage==1.19.1',
-        'ndg-httpsclient==0.5.1',
-        'pyasn1==0.4.8',
-        'pyopenssl==19.1.0',
-        'requests==2.23.0',
-      ],
-      entry_points={
-        'console_scripts': [
-          'gsecrets = gsecrets.cli:cli',
-        ]
-      },
-      cmdclass={
-        'upload': UploadCommand,
-      },
+here = os.path.abspath(os.path.dirname(__file__))
+
+# Import the README and use it as the long-description.
+with io.open(os.path.join(here, "README.rst"), encoding="utf-8") as f:
+    long_description = "\n" + f.read()
+
+
+# Load the package's __version__.py module as a dictionary.
+about = {}
+with open(os.path.join(here, NAME, "__version__.py")) as f:
+    exec(f.read(), about)
+
+setup(
+    name="gsecrets",
+    version=about["__version__"],
+    description=about["__description__"],
+    long_description=long_description,
+    author=about["__author__"],
+    author_email=about["__author_email__"],
+    url=about["__url__"],
+    packages=find_packages(),
+    license=about["__license__"],
+    install_requires=[
+        "click==7.0",
+        "google-api-python-client==1.7.11",
+        "google-cloud-storage==1.19.1",
+        "ndg-httpsclient==0.5.1",
+        "pyasn1==0.4.8",
+        "pyopenssl==19.1.0",
+        "requests==2.23.0",
+    ],
+    entry_points={"console_scripts": ["gsecrets = gsecrets.cli:cli"]},
+    cmdclass={"upload": UploadCommand},
 )
